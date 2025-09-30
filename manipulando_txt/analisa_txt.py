25f139be1d8c2c8cccbd8719fed77aee4945078e
unknown <josukeparte8@gmail.com>
2025-09-30 18:59:02 -0300

    Mais exercícios

---
+++ manipulando_txt/analisa_txt.py
@@ -0,0 +1,7 @@
+nome = str(input('Digite seu nome:')).strip()
+print('Analisando seu nome...')
+print('Seu nome em maiúsculas é {}.'.format(nome.upper()))
+print('Seu nome em minúsculas é {}.'.format(nome.lower()))
+print('Seu nome tem ao todo {:} letras.'.format(len(nome) - nome.count(' ')))
+nome1 = nome.split()
+print('Seu 1º nome {} tem {} letras.'.format(nome1[0] ,len(nome1[0])))
---
+++ manipulando_txt/separando.py
@@ -0,0 +1,6 @@
+num = input('Informe um número de 4 digitos: ').strip()
+print('Analisando o número {}'.format(num))
+print('Unidade: ',num[3])
+print('Dezena: ',num[2])
+print('Centena: ',num[1])
+print('Milhar: ',num[0])
